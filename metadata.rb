bcadf19dc7bde8fb3038d69a8edb34b1b503ec31
Nick Gerakines <nick@gerakines.net>
2014-05-14 20:19:02 -0400

    Bumping version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nick@gerakines'
 license          'MIT'
 description      'Installs/Configures preview'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'ark'
 depends 'apt'
